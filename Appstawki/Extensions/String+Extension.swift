a78730bb46589f3624bdea9e01be3358ae937280
Rafal Korzynski <rafal.korzynski@nextory.com>
2024-10-25 19:51:49 +0200

    Testing new regexp builder

--- Appstawki/Extensions/String+Extension.swift
+++ Appstawki/Extensions/String+Extension.swift
@@ -6,12 +6,41 @@
 //
 
 import Foundation
+import RegexBuilder
 
 extension String {
     
     var isValidEmail: Bool {
-        let emailRegExp = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegExp)
-        return emailPredicate.evaluate(with: self)
+        //let emailRegExp = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        //let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegExp)
+        //return emailPredicate.evaluate(with: self)
+        
+        let emailRegExp = Regex {
+            OneOrMore {
+                CharacterClass(
+                    .anyOf("._%+-"),
+                    ("A"..."Z"),
+                    ("0"..."9"),
+                    ("a"..."z")
+                )
+            }
+            "@"
+            OneOrMore {
+                CharacterClass(
+                    .anyOf("-"),
+                    ("A"..."Z"),
+                    ("a"..."z"),
+                    ("0"..."9")
+                )
+            }
+            "."
+            Repeat(2...64) {
+                CharacterClass(
+                    ("A"..."Z"),
+                    ("a"..."z")
+                )
+            }
+        }
+        return wholeMatch(of: emailRegExp) != nil
     }
 }
